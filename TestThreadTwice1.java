b51954f3c079d74b14e6b02383bfe340036bac88
dhimaseka <dimaseka83@gmail.com>
2022-11-30 11:23:37 +0700

    ++

--- TestThreadTwice1.java
+++ TestThreadTwice1.java
@@ -3,7 +3,7 @@ public class TestThreadTwice1 extends Thread {
         System.out.println("Sedang Berjalan...");
     }
     public static void main(String[] args) {
-        TestThreadTwice t1 = new TestThreadTwice1();
+        TestThreadTwice1 t1 = new TestThreadTwice1();
         t1.start();
         t1.start();
     }
